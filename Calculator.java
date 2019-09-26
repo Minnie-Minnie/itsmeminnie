2ae9e6f58760903d82ec3aa92a8716feedcc253c
Minnie-Minnie <dixitakanksha9090@gmail.com>
2019-09-26 16:48:04 +0530

    Hi i am Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+package capjava;
+
+public class Calculator {
+	int cost;
+	void add() {
+		System.out.println("add() method");
+		
+	}
+	
+	void sub() {
+		System.out.println("sub() method");
+	}
+
+}
